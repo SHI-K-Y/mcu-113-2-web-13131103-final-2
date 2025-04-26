3f13c64453bcd31cf0092b0fa90b620257d8dff2
Eric <eric941106@gmail.com>
2025-04-26 14:15:13 +0800

    feat(nav): 購物車產品數項顯示設定

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
+  standalone: true,
 })
-export class NavBarComponent {}
+export class NavBarComponent implements OnInit {
+  cartItemCount = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe((items) => {
+      // Calculate the total quantity of items in the cart
+      this.cartItemCount = items.reduce((total, item) => total + item.quantity, 0);
+    });
+  }
+}
